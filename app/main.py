a06ef584dcb27fd6883aca3f186ccf98dd4937ca
trgiangdo <dtr.giang.1299@gmail.com>
2026-02-25 21:36:57 +0700

    feature: Respond to PING

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
