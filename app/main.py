a64a23136be2af5d082b7e83e6509791cab87200
trgiangdo <dtr.giang.1299@gmail.com>
2026-02-26 08:33:33 +0700

    feature: Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -5,7 +5,16 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+
+    while True:
+        BUFFER_SIZE_BYTES = 1024
+        data = connection.recv(BUFFER_SIZE_BYTES)
+        command = data.decode("utf-8")
+        print(f"Received command: {command}")
+
+        connection.sendall(b"+PONG\r\n")
+
+    connection.close()
 
 
 if __name__ == "__main__":
