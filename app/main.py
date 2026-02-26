4c2cb6a35151e72b091afb86aecefb35021f05f8
trgiangdo <dtr.giang.1299@gmail.com>
2026-02-26 21:35:55 +0700

    feature: Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 import socket
+import threading
+
+HOST = "localhost"
+PORT = 6379
+
+BUFFER_SIZE_BYTES = 1024
+
+
+def handle_connection(conn: socket.socket) -> None:
+    while True:
+        data = conn.recv(BUFFER_SIZE_BYTES)
+        command = data.decode("utf-8")
+        if not command:
+            break
+        print(f"Received command: {command}")
+
+        conn.sendall(b"+PONG\r\n")
+
+    conn.close()
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    with socket.create_server((HOST, PORT), reuse_port=True) as server_socket:
+        while True:
+            connection, _ = server_socket.accept()
+            threading.Thread(target=handle_connection, args=(connection,)).start()
 
-    connection, _ = server_socket.accept()
-
-    while True:
-        BUFFER_SIZE_BYTES = 1024
-        data = connection.recv(BUFFER_SIZE_BYTES)
-        command = data.decode("utf-8")
-        print(f"Received command: {command}")
-
-        connection.sendall(b"+PONG\r\n")
-
-    connection.close()
 
 
 if __name__ == "__main__":
